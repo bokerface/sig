1ed31c81b6bce14f79b75294f0d08483d0e1775b
DESKTOP-EB8EN4I\X240 <waskitodamar17@gmail.com>
2022-04-28 12:19:52 +0700

    28 april 2022 12:19

--- app/Http/Livewire/Admin/AdminExchange.php
+++ app/Http/Livewire/Admin/AdminExchange.php
@@ -9,7 +9,6 @@ use Livewire\WithPagination;
 
 class AdminExchange extends Component
 {
-<<<<<<< HEAD
     public $meta;
     public $click;
     public $search;
@@ -82,48 +81,4 @@ class AdminExchange extends Component
     {
         $this->status = $status;
     }
-=======
-    public $ids;
-    public $fullname;
-    public $student_id;
-    public $metass;
-    public $modalOpen;
-
-    public function detail($id)
-    {
-        $exchange =  DB::table('exchanges')
-            ->select('exchanges.student_id', 'v_students.fullname', 'exchanges.created_at')
-            ->where('exchanges.id', '=', $id)
-            ->leftJoin('v_students', 'exchanges.student_id', '=', 'v_students.studentid')
-            ->first();
-
-        $this->ids = $id;
-        $this->student_id = $exchange->student_id;
-        $this->fullname = $exchange->fullname;
-        $this->modalOpen = true;
-
-        $metas = DB::table('metas')
-            ->select('metas.*')
-            ->where('post_id', '=', $id)
-            ->where('post_type', '=', 'exchange')
-            ->get();
-
-        $this->metass = $metas;
-        // dd($metas);
-    }
-
-    public function render()
-    {
-        return view(
-            'livewire.admin.admin-exchange',
-            [
-                "exchanges" => DB::table('exchanges')
-                    ->select('exchanges.id', 'exchanges.student_id', 'v_students.fullname', 'exchanges.created_at', 'exchanges.status')
-                    // ->where('exchanges.id', '=', $id)
-                    ->leftJoin('v_students', 'exchanges.student_id', '=', 'v_students.studentid')
-                    ->latest()->get(),
-            ]
-        )->layout('components.admin.layouts');
-    }
->>>>>>> main
 }
