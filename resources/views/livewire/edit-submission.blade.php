dfaaf5a01472a0ec86ef918c8924dc83979bd83b
ito <waskitodamar17@gmail.com>
2022-06-07 11:52:15 +0700

    test

--- resources/views/livewire/edit-submission.blade.php
+++ resources/views/livewire/edit-submission.blade.php
@@ -7,7 +7,6 @@
         h3.judul span {
             font-size: 11px;
         }
-
     </style>
 
     <x-modal-title title="Edit Submission" bg="bgsd-gradient" />
@@ -16,22 +15,23 @@
 
         <div class="card">
             <div class="card-body">
+                test
                 @foreach($submission_field_values as $item)
-                    <div class="mb-2">
-                        <label for="inputfile" class="form-label @error('{{ $item->key }}') text-danger @enderror">
-                            {{ $item->label }}
-                            <span class="text-danger">*</span>
-                        </label>
+                <div class="mb-2">
+                    <label for="inputfile" class="form-label @error('{{ $item->key }}') text-danger @enderror">
+                        {{ $item->label }}
+                        <span class="text-danger">*</span>
+                    </label>
 
-                        <input type="{{ $item->type }}" wire:model="{{ $item->key }}"
-                            class="form-control @error('{{ $item->key }}') is-invalid @enderror">
+                    <input type="{{ $item->type }}" wire:model="{{ $item->key }}"
+                        class="form-control @error('{{ $item->key }}') is-invalid @enderror">
 
-                        <span class="text-muted">Filetype: .pdf</span>
+                    <span class="text-muted">Filetype: .pdf</span>
 
-                        @error('{{ $item->key }}')
-                            <p class="text-danger mb-2">{{ $message }}</p>
-                        @enderror
-                    </div>
+                    @error('{{ $item->key }}')
+                    <p class="text-danger mb-2">{{ $message }}</p>
+                    @enderror
+                </div>
                 @endforeach
             </div>
         </div>
