0cbb7e4ee6cba03ed22e054562f1392d102ef055
DESKTOP-EB8EN4I\X240 <waskitodamar17@gmail.com>
2022-07-13 15:17:00 +0700

    13 juli 2022 15:16

--- resources/views/livewire/letter-index.blade.php
+++ resources/views/livewire/letter-index.blade.php
@@ -9,7 +9,7 @@
 
         <a href="{{ url('letter-recommendation-passport') }}"
             data-menu="menu-letter-recommendation-passport"
-            class="btn btn-sigov-pink text-left dark sdcustom-btnicon bgsd-circleimg-small btn-margins">
+            class="btn-sigov-pink text-left dark sdcustom-btnicon bgsd-circleimg-small btn-margins">
             <span class="icon d-flex justify-content-center align-items-center">
                 <i class="icon-passport text-center"></i>
             </span>
@@ -17,7 +17,7 @@
         </a>
 
         <a href="{{ url('letter-active-student') }}" data-menu="menu-letter-active-student"
-            class="btn btn-sigov-pink text-left dark sdcustom-btnicon bgsd-circleimg-small btn-margins">
+            class="btn-sigov-pink text-left dark sdcustom-btnicon bgsd-circleimg-small btn-margins">
             <span class="icon d-flex justify-content-center align-items-center">
                 <i class="icon-file-text text-center"></i>
             </span>
@@ -25,7 +25,7 @@
         </a>
 
         <a href="{{ url('letter-internship-program') }}" data-menu="menu-letter-internship-program"
-            class="btn btn-sigov-pink text-left dark sdcustom-btnicon bgsd-circleimg-small btn-margins">
+            class="btn-sigov-pink text-left dark sdcustom-btnicon bgsd-circleimg-small btn-margins">
             <span class="icon d-flex justify-content-center align-items-center">
                 <i class="icon-file-text text-center"></i>
             </span>
@@ -33,7 +33,7 @@
         </a>
 
         <a href="{{ url('letter-dispensation') }}" data-menu="menu-letter-dispensation-payment"
-            class="btn btn-sigov-pink text-left dark sdcustom-btnicon bgsd-circleimg-small btn-margins">
+            class="btn-sigov-pink text-left dark sdcustom-btnicon bgsd-circleimg-small btn-margins">
             <span class="icon d-flex justify-content-center align-items-center">
                 <i class="icon-file-text text-center"></i>
             </span>
