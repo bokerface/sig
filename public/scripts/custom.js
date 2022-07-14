b68e606069bf0b63d8ebb25db028e2709c2e92cb
DESKTOP-EB8EN4I\X240 <waskitodamar17@gmail.com>
2022-07-14 13:58:59 +0700

    pwa setting

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -595,110 +595,110 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
         //PWA Settings
-        // if(isPWA === true){
-        //     var checkPWA = document.getElementsByTagName('html')[0];
-        //     if(!checkPWA.classList.contains('isPWA')){
-        //         if ('serviceWorker' in navigator) {
-        //           window.addEventListener('load', function() {
-        //             navigator.serviceWorker.register(pwaLocation, {scope: pwaScope}).then(function(registration){registration.update();})
-        //           });
-        //         }
+        if(isPWA === true){
+            var checkPWA = document.getElementsByTagName('html')[0];
+            if(!checkPWA.classList.contains('isPWA')){
+                if ('serviceWorker' in navigator) {
+                  window.addEventListener('load', function() {
+                    navigator.serviceWorker.register(pwaLocation, {scope: pwaScope}).then(function(registration){registration.update();})
+                  });
+                }
 
-        //         //Setting Timeout Before Prompt Shows Again if Dismissed
-        //         var hours = pwaRemind * 1; // Reset when storage is more than 24hours
-        //         var now = Date.now();
-        //         var setupTime = localStorage.getItem(pwaName+'-PWA-Timeout-Value');
-        //         if (setupTime == null) {
-        //             localStorage.setItem(pwaName+'-PWA-Timeout-Value', now);
-        //         } else if (now - setupTime > hours*60*60*1000) {
-        //             localStorage.removeItem(pwaName+'-PWA-Prompt')
-        //             localStorage.setItem(pwaName+'-PWA-Timeout-Value', now);
-        //         }
+                //Setting Timeout Before Prompt Shows Again if Dismissed
+                var hours = pwaRemind * 1; // Reset when storage is more than 24hours
+                var now = Date.now();
+                var setupTime = localStorage.getItem(pwaName+'-PWA-Timeout-Value');
+                if (setupTime == null) {
+                    localStorage.setItem(pwaName+'-PWA-Timeout-Value', now);
+                } else if (now - setupTime > hours*60*60*1000) {
+                    localStorage.removeItem(pwaName+'-PWA-Prompt')
+                    localStorage.setItem(pwaName+'-PWA-Timeout-Value', now);
+                }
 
 
-        //         const pwaClose = document.querySelectorAll('.pwa-dismiss');
-        //         pwaClose.forEach(el => el.addEventListener('click',e =>{
-        //             const pwaWindows = document.querySelectorAll('#menu-install-pwa-android, #menu-install-pwa-ios');
-        //             for(let i=0; i < pwaWindows.length; i++){pwaWindows[i].classList.remove('menu-active');}
-        //             localStorage.setItem(pwaName+'-PWA-Timeout-Value', now);
-        //             localStorage.setItem(pwaName+'-PWA-Prompt', 'install-rejected');
-        //             console.log('PWA Install Rejected. Will Show Again in '+ (pwaRemind)+' Days')
-        //         }));
+                const pwaClose = document.querySelectorAll('.pwa-dismiss');
+                pwaClose.forEach(el => el.addEventListener('click',e =>{
+                    const pwaWindows = document.querySelectorAll('#menu-install-pwa-android, #menu-install-pwa-ios');
+                    for(let i=0; i < pwaWindows.length; i++){pwaWindows[i].classList.remove('menu-active');}
+                    localStorage.setItem(pwaName+'-PWA-Timeout-Value', now);
+                    localStorage.setItem(pwaName+'-PWA-Prompt', 'install-rejected');
+                    console.log('PWA Install Rejected. Will Show Again in '+ (pwaRemind)+' Days')
+                }));
 
-        //         //Trigger Install Prompt for Android
-        //         const pwaWindows = document.querySelectorAll('#menu-install-pwa-android, #menu-install-pwa-ios');
-        //         if(pwaWindows.length){
-        //             if (isMobile.Android()) {
-        //                 if (localStorage.getItem(pwaName+'-PWA-Prompt') != "install-rejected") {
-        //                     function showInstallPrompt() {
-        //                         setTimeout(function(){
-        //                             if (!window.matchMedia('(display-mode: fullscreen)').matches) {
-        //                                 console.log('Triggering PWA Window for Android')
-        //                                 document.getElementById('menu-install-pwa-android').classList.add('menu-active');
-        //                                 document.querySelectorAll('.menu-hider')[0].classList.add('menu-active');
-        //                             }
-        //                         },3500);
-        //                     }
-        //                     var deferredPrompt;
-        //                     window.addEventListener('beforeinstallprompt', (e) => {
-        //                         e.preventDefault();
-        //                         deferredPrompt = e;
-        //                         showInstallPrompt();
-        //                     });
-        //                 }
-        //                 const pwaInstall = document.querySelectorAll('.pwa-install');
-        //                 pwaInstall.forEach(el => el.addEventListener('click', e => {
-        //                     deferredPrompt.prompt();
-        //                     deferredPrompt.userChoice
-        //                         .then((choiceResult) => {
-        //                             if (choiceResult.outcome === 'accepted') {
-        //                                 console.log('Added');
-        //                             } else {
-        //                                 localStorage.setItem(pwaName+'-PWA-Timeout-Value', now);
-        //                                 localStorage.setItem(pwaName+'-PWA-Prompt', 'install-rejected');
-        //                                 setTimeout(function(){
-        //                                     if (!window.matchMedia('(display-mode: fullscreen)').matches) {
-        //                                         document.getElementById('menu-install-pwa-android').classList.remove('menu-active');
-        //                                         document.querySelectorAll('.menu-hider')[0].classList.remove('menu-active');
-        //                                     }
-        //                                 },50);
-        //                             }
-        //                             deferredPrompt = null;
-        //                         });
-        //                 }));
-        //                 window.addEventListener('appinstalled', (evt) => {
-        //                     document.getElementById('menu-install-pwa-android').classList.remove('menu-active');
-        //                     document.querySelectorAll('.menu-hider')[0].classList.remove('menu-active');
-        //                 });
-        //             }
-        //             //Trigger Install Guide iOS
-        //             if (isMobile.iOS()) {
-        //                 if (localStorage.getItem(pwaName+'-PWA-Prompt') != "install-rejected") {
-        //                     setTimeout(function(){
-        //                         if (!window.matchMedia('(display-mode: fullscreen)').matches) {
-        //                             console.log('Triggering PWA Window for iOS');
-        //                             document.getElementById('menu-install-pwa-ios').classList.add('menu-active');
-        //                             document.querySelectorAll('.menu-hider')[0].classList.add('menu-active');
-        //                         }
-        //                     },3500);
-        //                 }
-        //             }
-        //         }
-        //     }
-        //     checkPWA.setAttribute('class','isPWA');
-        // }
+                //Trigger Install Prompt for Android
+                const pwaWindows = document.querySelectorAll('#menu-install-pwa-android, #menu-install-pwa-ios');
+                if(pwaWindows.length){
+                    if (isMobile.Android()) {
+                        if (localStorage.getItem(pwaName+'-PWA-Prompt') != "install-rejected") {
+                            function showInstallPrompt() {
+                                setTimeout(function(){
+                                    if (!window.matchMedia('(display-mode: fullscreen)').matches) {
+                                        console.log('Triggering PWA Window for Android')
+                                        document.getElementById('menu-install-pwa-android').classList.add('menu-active');
+                                        document.querySelectorAll('.menu-hider')[0].classList.add('menu-active');
+                                    }
+                                },3500);
+                            }
+                            var deferredPrompt;
+                            window.addEventListener('beforeinstallprompt', (e) => {
+                                e.preventDefault();
+                                deferredPrompt = e;
+                                showInstallPrompt();
+                            });
+                        }
+                        const pwaInstall = document.querySelectorAll('.pwa-install');
+                        pwaInstall.forEach(el => el.addEventListener('click', e => {
+                            deferredPrompt.prompt();
+                            deferredPrompt.userChoice
+                                .then((choiceResult) => {
+                                    if (choiceResult.outcome === 'accepted') {
+                                        console.log('Added');
+                                    } else {
+                                        localStorage.setItem(pwaName+'-PWA-Timeout-Value', now);
+                                        localStorage.setItem(pwaName+'-PWA-Prompt', 'install-rejected');
+                                        setTimeout(function(){
+                                            if (!window.matchMedia('(display-mode: fullscreen)').matches) {
+                                                document.getElementById('menu-install-pwa-android').classList.remove('menu-active');
+                                                document.querySelectorAll('.menu-hider')[0].classList.remove('menu-active');
+                                            }
+                                        },50);
+                                    }
+                                    deferredPrompt = null;
+                                });
+                        }));
+                        window.addEventListener('appinstalled', (evt) => {
+                            document.getElementById('menu-install-pwa-android').classList.remove('menu-active');
+                            document.querySelectorAll('.menu-hider')[0].classList.remove('menu-active');
+                        });
+                    }
+                    //Trigger Install Guide iOS
+                    if (isMobile.iOS()) {
+                        if (localStorage.getItem(pwaName+'-PWA-Prompt') != "install-rejected") {
+                            setTimeout(function(){
+                                if (!window.matchMedia('(display-mode: fullscreen)').matches) {
+                                    console.log('Triggering PWA Window for iOS');
+                                    document.getElementById('menu-install-pwa-ios').classList.add('menu-active');
+                                    document.querySelectorAll('.menu-hider')[0].classList.add('menu-active');
+                                }
+                            },3500);
+                        }
+                    }
+                }
+            }
+            checkPWA.setAttribute('class','isPWA');
+        }
 
 
         // //End of isPWA
-        // if(pwaNoCache === true){
-        //     caches.delete('workbox-runtime').then(function() {});
-        //     sessionStorage.clear()
-        //     caches.keys().then(cacheNames => {
-        //       cacheNames.forEach(cacheName => {
-        //         caches.delete(cacheName);
-        //       });
-        //     });
-        // }
+        if(pwaNoCache === true){
+            caches.delete('workbox-runtime').then(function() {});
+            sessionStorage.clear()
+            caches.keys().then(cacheNames => {
+              cacheNames.forEach(cacheName => {
+                caches.delete(cacheName);
+              });
+            });
+        }
 
        
     }
