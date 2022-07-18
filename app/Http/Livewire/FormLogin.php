da30cdccc604a5caf2772dceab69a6104e3323d5
DESKTOP-EB8EN4I\X240 <waskitodamar17@gmail.com>
2022-07-18 15:57:33 +0700

    18 juli 20222 15:57 -perbaikan login mahasiswa

--- app/Http/Livewire/FormLogin.php
+++ app/Http/Livewire/FormLogin.php
@@ -57,24 +57,47 @@ class FormLogin extends Component
             // dd($user);
 
             if ($user) {
-                $user_data = [
-                    // "user_id" =>  $user->studentid,
-                    // "fullname" =>  $user->fullname,
-                    // "dateofbirth" =>  $user->dateofbirth,
-                    // "placeofbirth" =>  $user->placeofbirth,
-                    // "email" => $user->email,
-                    "user_id" =>  $user->STUDENTID,
-                    "fullname" =>  $user->FULLNAME,
-                    "dateofbirth" =>  $user->DATEOFBIRTH,
-                    "placeofbirth" =>  $user->PLACEOFBIRTH,
-                    "email" => $user->EMAIL,
-                    "role" => 3,
-                    "isLoggedIn" => true
-                ];
 
-                Session::put("user_data", $user_data);
+                $year = date('Y');
+                $thn_ajaran = (date('n') <= 6) ? $year - 1 : $year;
+                $first_semester = [8, 9, 10, 11, 12, 1];
+                $second_semester = [2, 3, 4, 5, 6, 7];
+                if (in_array(date('n'), $first_semester)) {
+                    $current_semester = 1;
+                } elseif (in_array(date('n'), $second_semester)) {
+                    $current_semester = 2;
+                }
 
-                return redirect()->to(route('home'));
+                $send =
+                    Http::withHeaders([])->post('https://krs.umy.ac.id/WebApi/Info/GetStatusAktif', [
+                        'ClientId' => 'SiGov',
+                        'Credential' => 'F3BC28F09FC5F33B453030D763B06369EC708AED70634A5901C93CF7244FFB35',
+                        'NIM' => $user->STUDENTID,
+                        'TahunAjaran' => 2018,
+                        'Semester' => $current_semester
+                    ])->object();
+
+                if ($send->Status == 1) {
+                    $user_data = [
+                        // "user_id" =>  $user->studentid,
+                        // "fullname" =>  $user->fullname,
+                        // "dateofbirth" =>  $user->dateofbirth,
+                        // "placeofbirth" =>  $user->placeofbirth,
+                        // "email" => $user->email,
+                        "user_id" =>  $user->STUDENTID,
+                        "fullname" =>  $user->FULLNAME,
+                        "dateofbirth" =>  $user->DATEOFBIRTH,
+                        "placeofbirth" =>  $user->PLACEOFBIRTH,
+                        "email" => $user->EMAIL,
+                        "role" => 3,
+                        "isLoggedIn" => true
+                    ];
+
+                    Session::put("user_data", $user_data);
+
+                    return redirect()->to(route('home'));
+                }
+                return redirect()->back()->with('error', $send->Message);
             } else {
                 return back()->with('error', 'You are not allowed to access this apps.');
             }
